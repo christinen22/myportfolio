08b756a7e7e39d6c63b477597ea612a20d3fed26
christinen22 <christine.elin.louise@gmail.com>
2023-08-02 11:17:20 +0200

    Changed hero

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -1,50 +1,31 @@
-import React from "react";
-import HeroImg from "../assets/hero-img.png";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Chrissy from "../assets/chrissy.png";
 
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <section className="px-5 py-32">
-      <div className="hero-info pb-5 md:pb-0">
-        <div className="hero-img">
-          <h1 className="text-4xl lg:text-6xl">
-            Hi, <br />I am <span className="text-accent">C</span>hristine <br />
-            Frontend Developer
-          </h1>
-
-          <img src={HeroImg} alt="illustration" className="hero-img-img" />
-        </div>
-      </div>
-
-      <p className="py-5">
-        I am proficient in JavaScript, Typescript, React.js, Laravel and
-        learning more...
-      </p>
-
-      <div className="flex py-5 ">
-        <a
-          href="https://github.com/christinen22"
-          className="pr-4 inline-block text-accent hover:text-white"
-        >
-          {" "}
-          <FaGithub size={40} />{" "}
-        </a>
-        <a
-          href="https://www.linkedin.com/in/christine-nilsson-82536267/"
-          className="pr-4 inline-block text-accent hover:text-white"
-        >
-          {" "}
-          <FaLinkedinIn size={40} />{" "}
-        </a>
-      </div>
-
-      <a
-        href="/#projects"
-        className=" btn bg-accent  border-2 border-[#7477FF] px-6 py-3 hover:bg-transparent"
-      >
-        See Projects
-      </a>
+    <section className="hero-section" id="home">
+      <Container className="hero-container">
+        <Row className="align-items-center">
+          <Col lg={6} md={6} sm={12}>
+            <div className="hero-content">
+              <h1 className="hero-title">Hi, I'm Christine.</h1>
+              <p className="hero-subtitle">
+                I am in love with the world of code.
+              </p>
+              <Button href="#contact" variant="primary" className="hero-button">
+                Get in Touch
+              </Button>
+            </div>
+          </Col>
+          <Col lg={6} md={6} sm={12}>
+            <div className="hero-image">
+              <img src={Chrissy} alt="illustration" className="chrissy-img" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
